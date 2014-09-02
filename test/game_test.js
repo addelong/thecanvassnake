e93e69162b84f937d782271f7eecc9e7994ac280
addelong <github@alandelong.com>
2014-09-02 02:36:18 -0400

    move the snake around

--- test/game_test.js
+++ test/game_test.js
@@ -4,6 +4,7 @@ var canvas = require('../lib/canvas.js');
 var expect = chai.expect;
 var domHelper = require('./domhelper.js');
 domHelper.setup();
+addEventListener = function(){}; // jshint ignore:line
 var game = require('../lib/game.js');
 
 var canvasStub = sinon.stub(canvas, 'clearCanvas');
@@ -12,7 +13,7 @@ describe('The game', function(){
 	it('should initialize game settings when reset', function(){
 		game.reset({});
 		expect(game.getSettings().score).to.equal(0);
-		expect(game.getSettings().speed).to.equal(10);
+		expect(game.getSettings().speed).to.equal(20);
 	});
 	it('should clear the canvas when reset', function(){
 		game.reset({});
--- test/snake_test.js
+++ test/snake_test.js
@@ -23,3 +23,33 @@ describe('The initial snake', function(){
 		expect(snake.getSnake().pieces[4].y).to.equal(0);
 	});
 });
+
+describe('Moving the snake', function(){
+	beforeEach(function(){
+		snake.initialize();
+	});
+	it('should increase the y coordinate of the first piece if moving down', function(){
+		snake.getSnake().direction = "down";
+		var headY = snake.getSnake().pieces[0].y;
+		snake.move();
+		expect(snake.getSnake().pieces[0].y).to.equal(++headY);
+	});
+	it('should increase the x coordinate of the first piece if moving right', function(){
+		snake.getSnake().direction = "right";
+		var headX = snake.getSnake().pieces[0].x;
+		snake.move();
+		expect(snake.getSnake().pieces[0].x).to.equal(++headX);
+	});
+	it('should decrease the y coordinate of the first piece if moving up', function(){
+		snake.getSnake().direction = "up";
+		var headY = snake.getSnake().pieces[0].y;
+		snake.move();
+		expect(snake.getSnake().pieces[0].y).to.equal(--headY);
+	});
+	it('should decrease the x coordinate of the first piece if moving left', function(){
+		snake.getSnake().direction = "left";
+		var headX = snake.getSnake().pieces[0].x;
+		snake.move();
+		expect(snake.getSnake().pieces[0].x).to.equal(--headX);
+	});
+});
