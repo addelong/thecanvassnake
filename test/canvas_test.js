5ce5a6c2f38019d713dfeda580e1cfee71c1427d
addelong <github@alandelong.com>
2014-09-01 22:24:54 -0400

    refactoring

--- test/canvas_test.js
+++ test/canvas_test.js
@@ -1,31 +1,38 @@
 var canvas = require('../lib/canvas.js');
 var chai = require('chai');
 var sinon = require('sinon');
+var _ = require('underscore');
 var expect = chai.expect;
 
+var dom = {};
 
 describe('The canvas', function() {
-
+	beforeEach(function(){
+		_.extend(dom, {
+						canvasElement: { width: 5, height: 5, style: {} },
+						context: { 
+									scale: function () {},
+						  		 	fillRect: function () {} 
+						  		 },
+						getScaleFactor: function () {return 2;}
+					});
+	});
 	it('should be scaled if the pixel density is > 1', function() {
-		var canvasElement = { width: 5, height: 5, style: {} };
-		var context = {scale: function () {} };
-		var stub = sinon.stub(context, "scale");
-		canvas.scale(canvasElement, context, 2);
+		var stub = sinon.stub(dom.context, "scale");
+		canvas.scale(dom);
 
-		expect(canvasElement.width).to.equal(10);
-		expect(canvasElement.height).to.equal(10);
-		expect(canvasElement.style.width).to.equal('5px');
-		expect(canvasElement.style.height).to.equal('5px');
+		expect(dom.canvasElement.width).to.equal(10);
+		expect(dom.canvasElement.height).to.equal(10);
+		expect(dom.canvasElement.style.width).to.equal('5px');
+		expect(dom.canvasElement.style.height).to.equal('5px');
 		expect(stub).to.have.been.called;
 	});
 
 	it('should make the canvas background black', function() {
-		var canvasElement = { width: 5, height: 5};
-		var context = {fillRect: function () {} };
-		var stub = sinon.stub(context, "fillRect");
-		canvas.fill(canvasElement, context);
+		var stub = sinon.stub(dom.context, "fillRect");
+		canvas.fill(dom);
 
-		expect(context.fillStyle).to.equal("black");
+		expect(dom.context.fillStyle).to.equal("black");
 		expect(stub.fillRect).to.have.been.called;
 	});
 
